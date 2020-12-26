20cc527a1475e2ba113b670e83fd084a66a1ecfc
ialexanderbrito <xanderzinho26@gmail.com>
2020-12-26 13:37:42 -0300

    🔧 Atualizado ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,8 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'no-param-reassign': 'off',
     'no-console': ["error", { allow: ["tron"] }],
-    semi: "off"
+    semi: "off",
+    'jsx-a11y/anchor-is-valid': 'off',
+    'jsx-a11y/anchor-has-content': 'off'
   },
 };
