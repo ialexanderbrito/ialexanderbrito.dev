28f6f1718825bdc48a2f3b52612612178aeb3031
ialexanderbrito <xanderzinho26@gmail.com>
2021-08-18 13:27:48 -0300

    :sparkles: PWA

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorkerRegistration.register();
+
+reportWebVitals();
