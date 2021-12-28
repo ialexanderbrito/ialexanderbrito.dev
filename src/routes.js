3fcbd6980b8735a05643268128c870979dd7fbdc
ialexanderbrito <xanderzinho26@gmail.com>
2021-12-28 10:42:07 -0300

    🚚 Novas rotas

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes as Switch } from 'react-router-dom';
 
-import About from 'pages/About';
-import Bio from 'pages/Bio';
-import Home from 'pages/Home';
-import NotFound from 'pages/NotFound';
-import Projects from 'pages/Projects';
+import { About } from 'pages/About';
+import { Bio } from 'pages/Bio';
+import { Home } from 'pages/Home';
+import { Live } from 'pages/Live';
+import { NotFound } from 'pages/NotFound';
+import { Projects } from 'pages/Projects';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/bio" component={Bio} />
-        <Route path="*" component={NotFound} />
+        <Route path="/" exact element={<Home />} />
+        <Route path="/sobre" element={<About />} />
+        <Route path="/projetos" element={<Projects />} />
+        <Route path="/bio" element={<Bio />} />
+        <Route path="/live" element={<Live />} />
+        <Route path="*" element={<NotFound />} />
       </Switch>
     </BrowserRouter>
   );
