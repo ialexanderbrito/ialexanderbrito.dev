6a96593efc4f274afce49750470192281168af07
ialexanderbrito <xanderzinho26@gmail.com>
2021-12-30 19:01:19 -0300

    ✨ Adicionado novas funções portfólio

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,57 +1,53 @@
 import { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet';
-// import Skeleton from 'react-loading-skeleton';
+import { IoArrowBackOutline } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
 
-import Portfolio from 'components/Portfolio';
-import Skeleton from 'components/Skeleton';
+import { Portfolio } from 'components/Portfolio';
+import { SkeletonComponent } from 'components/Skeleton';
 
-import './styles.css';
-
-export default function Projects() {
+export function Projects() {
   const [loading, setLoading] = useState(false);
+
+  const delay = 800;
 
   useEffect(() => {
     setLoading(true);
 
     setTimeout(() => {
       setLoading(false);
-    }, 800);
+    }, delay);
   }, []);
 
   return (
     <>
       <Helmet>
-        <title>Alexander · Projetos</title>
+        <title>Projetos | Alexander - Front-end Developer</title>
       </Helmet>
       {loading ? (
-        <>
-          <main className="l-main">
-            <section className="work section" id="work">
-              <div className="about__back">
-                <Link to="/">
-                  <i className="bx bx-arrow-back" />
-                </Link>
-              </div>
-              <h2 className="section-title">Projetos</h2>
-              <Skeleton />
-            </section>
-          </main>
-        </>
+        <main className="l-main">
+          <section className="work section" id="work">
+            <div className="about-back">
+              <Link to="/">
+                <IoArrowBackOutline color="#3F8C6A" />
+              </Link>
+            </div>
+            <h2 className="section-title">Projetos</h2>
+            <SkeletonComponent />
+          </section>
+        </main>
       ) : (
-        <>
-          <main className="l-main">
-            <section className="work section" id="work">
-              <div className="about__back">
-                <Link to="/">
-                  <i className="bx bx-arrow-back" />
-                </Link>
-              </div>
-              <h2 className="section-title">Projetos</h2>
-              <Portfolio />
-            </section>
-          </main>
-        </>
+        <main className="l-main">
+          <section className="work section" id="work">
+            <div className="about-back">
+              <Link to="/">
+                <IoArrowBackOutline color="#3F8C6A" />
+              </Link>
+            </div>
+            <h2 className="section-title">Projetos</h2>
+            <Portfolio />
+          </section>
+        </main>
       )}
     </>
   );
