4a1c1d773e3471d11bece1e0800bffe3f654a718
ialexanderbrito <xanderzinho26@gmail.com>
2022-03-14 16:56:03 -0300

    ✨ Adicionado data

---
+++ data/setup.js
@@ -0,0 +1,155 @@
+const categories = [
+  {
+    name: 'Equipamentos',
+    items: [
+      {
+        title: 'Hackintosh - i5 10600K - Placa Mãe Gigabyte Z490M Gaming X - 32GB de RAM Kllisre',
+      },
+      {
+        title: 'Placa de Vídeo RX 580 - WaterCooler 240mm Gamdias M2 - Gabinete Kazan RGB',
+      },
+      {
+        title: 'Monitor - LG 29" UltraWide (29WK600)',
+        url: 'https://www.kabum.com.br/produto/97920',
+      },
+      {
+        title: 'Monitor - AOC 27" 144Hz (27G2/BK)',
+        url: 'https://www.kabum.com.br/produto/111161',
+      },
+      {
+        title: 'Mouse - Logitech G203 LightSync',
+        url: 'https://www.kabum.com.br/produto/112948',
+      },
+      {
+        title: 'Teclado - Logitech G213',
+        url: 'https://www.kabum.com.br/produto/107334',
+      },
+      {
+        title: 'Hub: TP-Link de 7 portas (UH700)',
+        url: 'https://www.kabum.com.br/produto/129463',
+      },
+      // {
+      //   title: 'Webcam - Logitech C922 Pro',
+      //   url: 'https://amzn.to/3vUa83T',
+      // },
+      {
+        title: 'Headset - Havit HV-H2002D',
+        url: 'https://www.kabum.com.br/produto/102770',
+      },
+      {
+        title: 'Microfone - Vedo BM800',
+        url: 'https://www.americanas.com.br/produto/4052539083',
+      },
+      {
+        title: 'Mesa - Kuadra Nogal 1.50M',
+        url: 'https://www.americanas.com.br/produto/58709685',
+      },
+      {
+        title: 'Cadeira - Husky Gaming Tempest 500',
+        url: 'https://www.kabum.com.br/produto/134180',
+      },
+    ],
+  },
+  {
+    name: 'Código',
+    items: [
+      {
+        title: 'Editor - VS Code',
+        url: 'https://code.visualstudio.com',
+      },
+      {
+        title: 'Tema - Omni Owl',
+        url: 'https://marketplace.visualstudio.com/items?itemName=guilhermerodz.omni-owl',
+      },
+      {
+        title: 'Fonte - JetBrains Mono',
+        url: 'https://www.jetbrains.com/pt-br/lp/mono/',
+      },
+    ],
+  },
+  {
+    name: 'Terminal',
+    items: [
+      {
+        title: 'Hyper',
+        url: 'https://hyper.is',
+      },
+      {
+        title: 'Tema - Omni',
+        url: 'https://github.com/getomni/hyper',
+      },
+      {
+        title: 'Oh My ZSH',
+        url: 'https://ohmyz.sh',
+      },
+      {
+        title: 'Fig',
+        url: 'http://fig.io',
+      },
+    ],
+  },
+  {
+    name: 'Apps',
+    items: [
+      {
+        title: 'Raycast',
+        url: 'https://www.raycast.com/',
+      },
+      {
+        title: 'Magnet',
+        url: 'https://apps.apple.com/br/app/magnet/id441258766?mt=12',
+      },
+      {
+        title: 'Figma',
+        url: 'https://figma.com',
+      },
+      {
+        title: 'Photoshop',
+        url: 'https://www.adobe.com/br/products/photoshop.html',
+      },
+      {
+        title: 'Insomnia',
+        url: 'https://insomnia.rest/download',
+      },
+      {
+        title: 'OBS',
+        url: 'https://obsproject.com/pt-br/download',
+      },
+      {
+        title: 'DBeaver',
+        url: 'https://dbeaver.io/download/',
+      },
+      {
+        title: 'Mini-video-me',
+        url: 'https://github.com/maykbrito/mini-video-me/',
+      },
+      {
+        title: 'Spotify',
+        url: 'https://www.spotify.com/br/',
+      },
+      {
+        title: 'GitKraken',
+        url: 'https://www.gitkraken.com/download',
+      },
+      {
+        title: 'CopyClip',
+        url: 'https://apps.apple.com/br/app/copyclip-clipboard-history/id595191960?mt=12',
+      },
+    ],
+  },
+  {
+    name: 'Serviços',
+    items: [
+      {
+        title: 'Google Domains',
+        url: 'https://domains.google',
+      },
+      {
+        title: 'Netlify',
+        url: 'https://netlify.com/',
+      },
+    ],
+  },
+]
+
+export default categories
