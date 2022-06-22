9451b66e1933e5f63afeb3e0325fcba3cdc147be
ialexanderbrito <xanderzinho26@gmail.com>
2022-06-22 01:27:02 -0300

    ✨ Atualizado URL IK

--- data/about.js
+++ data/about.js
@@ -2,14 +2,14 @@ const items = [
   {
     jobTitle: 'Desenvolvedor Front-end Pleno',
     company: 'IK Solution',
-    companyUrl: 'https://iksolution.com.br/',
+    companyUrl: 'https://ik.com.br/',
     startDate: '2021-10',
     location: 'São Paulo, SP',
   },
   {
     jobTitle: 'Desenvolvedor Front-end Júnior',
     company: 'IK Solution',
-    companyUrl: 'https://iksolution.com.br/',
+    companyUrl: 'https://ik.com.br/',
     startDate: '2020-12',
     endDate: '2021-10',
     location: 'São Paulo, SP',
