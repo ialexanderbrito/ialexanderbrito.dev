e78df4a456d9e7aae79ab3c25f147060f8bf0ada
ialexanderbrito <xanderzinho26@gmail.com>
2022-08-28 22:33:54 -0300

    🐛 Fix links setup

--- data/setup.js
+++ data/setup.js
@@ -4,28 +4,22 @@ const categories = [
     items: [
       {
         title: 'Placa Mãe Gigabyte Z490M Gaming X',
-        url: 'https://www.kabum.com.br/produto/112810/'
       },
       {
         title:
           'Processador Intel - i5 10600K',
-        url: 'https://www.kabum.com.br/produto/112993/'
       },
       {
         title: 'Mémoria Kllisre - 32GB de RAM',
-        url: 'https://pt.aliexpress.com/item/32961521712.html'
       },
       {
         title: 'Placa de Vídeo RX 580 Asus',
-        url: 'https://www.casasbahia.com.br/placa-de-video-asus-radeon-rx-580-dual-oc-8gb-gddr5-pci-express-1529352207/p/1529352207'
       },
       {
         title: 'WaterCooler - 240mm Gamdias M2',
-        url: 'https://www.kabum.com.br/produto/128322'
       },
       {
         title: 'Gabinete Kazan RGB',
-        url: 'https://www.amazon.com.br/Gabinete-Pichau-Lateral-frontal-Pgkz-01/dp/B07XH3GC47'
       }
     ],
   },
@@ -34,39 +28,30 @@ const categories = [
     items: [
       {
         title: 'Monitor - LG 29" UltraWide (29WK600)',
-        url: 'https://www.kabum.com.br/produto/97920',
       },
       {
         title: 'Monitor - AOC 27" 144Hz (27G2/BK)',
-        url: 'https://www.kabum.com.br/produto/111161',
       },
       {
-        title: 'Mouse - Logitech G603 sem Fio',
-        url: 'https://www.kabum.com.br/produto/',
+        title: 'Mouse - Logitech G603 Sem Fio LightSpeed',
       },
       {
         title: 'Mouse - Logitech G203 LightSync',
-        url: 'https://www.kabum.com.br/produto/112948',
       },
       {
         title: 'Teclado - Logitech G213',
-        url: 'https://www.kabum.com.br/produto/107334',
       },
       {
         title: 'Hub: TP-Link de 7 portas (UH700)',
-        url: 'https://www.kabum.com.br/produto/129463',
       },
-      // {
-      //   title: 'Webcam - Logitech C922 Pro',
-      //   url: 'https://amzn.to/3vUa83T',
-      // },
+      {
+        title: 'Webcam - Papalook PA930',
+      },
       {
         title: 'Headset - Havit HV-H2002D',
-        url: 'https://www.kabum.com.br/produto/102770',
       },
       {
         title: 'Microfone - Vedo BM800',
-        url: 'https://www.americanas.com.br/produto/4052539083',
       },
     ],
   },
@@ -75,20 +60,27 @@ const categories = [
     items: [
       {
         title: 'Mesa - Kuadra Nogal 1.50M',
-        url: 'https://www.americanas.com.br/produto/58709685',
       },
       {
         title: 'Cadeira - Husky Gaming Tempest 500',
-        url: 'https://www.kabum.com.br/produto/134180',
       },
       {
         title: 'Deskpad - Maphiro 90x40',
-        url: 'https://produto.mercadolivre.com.br/MLB-2173586210-mousepad-gamer-profissional-90x40-cm-desk-pad-alta-qualidade-_JM',
       },
       {
         title: 'Luminária de mesa',
-        url: 'https://www.americanas.com.br/produto/3923586151',
       },
+    ],
+  },
+  {
+    name: 'Automação',
+    items: [
+      {
+        title: 'Echo Dot(4ª Geração)',
+      },
+      {
+        title: 'Lampada - Yeelight 1S',
+      }
     ],
   },
   {
@@ -204,6 +196,10 @@ const categories = [
       {
         title: 'Netlify',
         url: 'https://netlify.com/',
+      },
+      {
+        title: 'Render',
+        url: 'https://render.com/',
       },
       {
         title: 'Zoho',
