ea6de4d87e054b5fde70d56da9362fef5b9049e9
ialexanderbrito <xanderzinho26@gmail.com>
2024-05-02 16:40:05 -0300

    ✨ Adicionado componente navbar

---
+++ src/components/mobile-nav.tsx
@@ -0,0 +1,76 @@
+import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { Button } from './ui/button';
+
+export function MobileNav() {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost">
+          <svg strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5">
+            <path
+              d="M3 5H11"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></path>
+            <path
+              d="M3 12H16"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></path>
+            <path
+              d="M3 19H21"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></path>
+          </svg>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left">
+        <SheetHeader>
+          <SheetTitle className="justify-start flex items-center">
+            <SheetClose asChild>
+              <Link href="/" passHref className="flex items-center justify-center gap-2">
+                <Image src="/favicon.png" width={24} height={24} alt="Logo" className="w-6 h-6 mb-2" />
+                <span className="font-bold mb-2">ialexanderbrito</span>
+              </Link>
+            </SheetClose>
+          </SheetTitle>
+
+          <ul className="space-y-3 my-4 pb-10 pl-8">
+            <li className="text-start">
+              <SheetClose asChild>
+                <Link href="/about" passHref>
+                  Sobre
+                </Link>
+              </SheetClose>
+            </li>
+            <li className="text-start">
+              <SheetClose asChild>
+                <Link href="/projects" passHref>
+                  Projetos
+                </Link>
+              </SheetClose>
+            </li>
+
+            <li className="text-start">
+              <SheetClose asChild>
+                <Link href="/tech" passHref>
+                  Tecnologias
+                </Link>
+              </SheetClose>
+            </li>
+          </ul>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import * as React from 'react';
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { MobileNav } from './mobile-nav';
+import { ModeToggle } from './mode-toggle';
+
+export function Navbar() {
+  return (
+    <>
+      <header className="mx-auto md:hidden max-w-5xl items-center justify-between gap-20 px-5 py-4 xl:px-0 flex-row md:flex-row flex">
+        <MobileNav />
+        <ModeToggle />
+      </header>
+      <header className="mx-auto hidden max-w-5xl items-center justify-between gap-20 px-5 py-4 xl:px-0 flex-row-reverse md:flex-row md:flex">
+        <Link href="/" passHref>
+          <Image src="/favicon.png" width={24} height={24} alt="Logo" className="w-6 h-6 mb-2" />
+        </Link>
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <Link href="/about" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>Sobre</NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/projects" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>Projetos</NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/tech" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>Tecnologias</NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+
+        <ModeToggle />
+      </header>
+    </>
+  );
+}
+
+const ListItem = React.forwardRef<React.ElementRef<'a'>, React.ComponentPropsWithoutRef<'a'>>(
+  ({ className, title, children, ...props }, ref) => (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
+            className,
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">{children}</p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  ),
+);
+ListItem.displayName = 'ListItem';
