cf7e122cb0ec00b967b9f9eae22becffa8457b0a
ialexanderbrito <xanderzinho26@gmail.com>
2024-05-02 16:42:01 -0300

    🔍️ Adicionado configuração de SEO

---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: 'Alexander',
+  url: 'https://ialexanderbrito.dev',
+  ogImage: 'https://ialexanderbrito/og.jpeg',
+  description:
+    'Um desenvolvedor front-end apaixonado por tecnologia e design, que adora compartilhar conhecimento e experiências!',
+  links: {
+    twitter: 'https://twitter.com/ialexanderbrito',
+    github: 'https://github.com/ialexanderbrito',
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
