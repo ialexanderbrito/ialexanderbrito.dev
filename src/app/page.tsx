ed44990b46969f7399f31a6505b02e05b641e428
ialexanderbrito <xanderzinho26@gmail.com>
2024-05-02 16:45:08 -0300

    ✨ Adicionado página principal

---
+++ src/app/page.tsx
@@ -0,0 +1,46 @@
+import { Metadata } from 'next';
+
+const thumbnail = {
+  url: 'https://ialexanderbrito.dev/og.jpeg',
+  width: 1200,
+  height: 630,
+  alt: 'Home',
+};
+
+const defaultMetadata = {
+  title: 'Alexander',
+  description: 'Opa, eu sou Alexander -- Front-end Developer & Mobile Developer',
+  images: [thumbnail],
+};
+
+export const metadata: Metadata = {
+  ...defaultMetadata,
+  twitter: {
+    ...defaultMetadata,
+    card: 'summary_large_image',
+    site: '@ialexanderbrito',
+    creator: '@ialexanderbrito',
+  },
+  openGraph: {
+    ...defaultMetadata,
+    type: 'website',
+    url: 'https://ialexanderbrito.dev',
+    siteName: 'Alexander',
+  },
+};
+
+export default function Home() {
+  return (
+    <main className="max-w-screen-lg mx-auto px-4">
+      <section className="h-[80vh] flex flex-col justify-center items-center">
+        <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold md:leading-relaxed text-center">
+          Opa, eu sou Alexander -- Front-end Developer & Mobile Developer
+        </h2>
+
+        <h2 className="text-md font-regular text-muted-foreground text-center">
+          um desenvolvedor de software apaixonado por interfaces e experiências incríveis.
+        </h2>
+      </section>
+    </main>
+  );
+}
