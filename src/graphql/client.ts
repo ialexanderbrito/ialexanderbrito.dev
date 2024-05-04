e79b2f3cfc84de7de0ce408f6eec5d70e4d49db5
ialexanderbrito <xanderzinho26@gmail.com>
2024-05-03 21:13:19 -0300

    ✨ Adicionado revalidate via variavel de ambiente

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -8,7 +8,7 @@ export const fetchHygraph = async (query: string) => {
     },
     body: JSON.stringify({ query }),
     next: {
-      revalidate: 60 * 60 * 24, // 24 hours
+      revalidate: Number(process.env.NEXT_PUBLIC_REVALIDADE_IN_SECONDS),
     },
   });
 
