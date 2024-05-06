27e45727ec7fbcf53c940e6407f9471661046874
ialexanderbrito <xanderzinho26@gmail.com>
2024-05-05 23:50:29 -0300

    ✨ Adicionado novo componente Analytics

---
+++ src/components/analytics.tsx
@@ -0,0 +1,46 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import Script from 'next/script';
+
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+const CLARITY_ID = process.env.NEXT_PUBLIC_CLARITY_ID;
+
+export function Analytics() {
+  return (
+    <>
+      {CLARITY_ID ? (
+        <Script id="clarity-script">
+          {`
+          (function (c, l, a, r, i, t, y) {
+            c[a] =
+              c[a] ||
+              function () {
+                (c[a].q = c[a].q || []).push(arguments);
+              };
+            t = l.createElement(r);
+            t.async = 1;
+            t.src = "https://www.clarity.ms/tag/" + i;
+            y = l.getElementsByTagName(r)[0];
+            y.parentNode.insertBefore(t, y);
+          })(window, document, "clarity", "script", "${CLARITY_ID}");
+        `}
+        </Script>
+      ) : null}
+      {GA_MEASUREMENT_ID ? (
+        <>
+          <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`} />
+          <Script id="google-analytics">
+            {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${GA_MEASUREMENT_ID}');
+      `}
+          </Script>
+        </>
+      ) : null}
+
+      <SpeedInsights />
+    </>
+  );
+}
