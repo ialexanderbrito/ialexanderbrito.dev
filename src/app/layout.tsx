7250d6231202027099b8d51ef6deb1a11e0e074a
ialexanderbrito <xanderzinho26@gmail.com>
2024-05-06 00:12:24 -0300

    ✨ Adicionado novas importações

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,16 @@
+import { EasterEggs } from '@/app/eastereggs';
+import { Analytics } from '@/components/analytics';
 import { ButtonTop } from '@/components/button-top';
-import Footer from '@/components/footer';
-import { GTag } from '@/components/gtag';
+import { Footer } from '@/components/footer';
 import { Navbar } from '@/components/navbar';
 import { PatternBackground } from '@/components/pattern-bg';
 import { ThemeProvider } from '@/components/theme-provider';
 import { siteConfig } from '@/config/site';
+import { ToasterProvider } from '@/context/toaster';
 import { Metadata } from 'next';
 import { Manrope } from 'next/font/google';
 import NextTopLoader from 'nextjs-toploader';
+
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -25,7 +28,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" suppressHydrationWarning className={fontSans.className}>
       <head>
         <link rel="icon" href="/favicon.png" sizes="any" />
-        <GTag />
+        <Analytics />
       </head>
       <body className="bg-background  antialiased">
         <PatternBackground variant="checkered" />
@@ -35,6 +38,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           {children}
         </ThemeProvider>
         <Footer />
+        <EasterEggs />
+        <ToasterProvider />
         <ButtonTop />
       </body>
     </html>
