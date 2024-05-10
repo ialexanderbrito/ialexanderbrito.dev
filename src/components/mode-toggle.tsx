d1757fcfedfd8ae9ca77df92a8819c9fb274a539
ialexanderbrito <xanderzinho26@gmail.com>
2024-05-10 16:59:09 -0300

    🔥 Removido tema "sistema"

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -9,23 +9,19 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { Monitor, Moon, Sun } from 'lucide-react';
+import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
 export function ModeToggle() {
-  const { setTheme, theme } = useTheme();
+  const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          {theme === 'light' && (
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          )}
-          {theme === 'dark' && (
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          )}
-          {theme === 'system' && <Monitor className="absolute h-[1.2rem] w-[1.2rem]" />}
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+
           <span className="sr-only">Alternar entre os temas claro e escuro</span>
         </Button>
       </DropdownMenuTrigger>
@@ -37,10 +33,6 @@ export function ModeToggle() {
         <DropdownMenuItem onClick={() => setTheme('dark')}>
           <Moon className="h-[1.2rem] w-[1.2rem] mr-2" />
           Escuro
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>
-          <Monitor className="h-[1.2rem] w-[1.2rem] mr-2" />
-          Sistema
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
