25ec408ca6eddbda052170bb2a18301ad4d764e0
ialexanderbrito <xanderzinho26@gmail.com>
2024-11-07 10:44:20 -0300

    🐛 Correção theme provider

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -3,8 +3,7 @@
 import * as React from 'react';
 
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import { type ThemeProviderProps } from 'next-themes/dist/types';
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({ children, ...props }: any) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
