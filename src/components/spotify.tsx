398e566dc4a11c7364d6dfb4a5a58603d24fda05
ialexanderbrito <xanderzinho26@gmail.com>
2024-11-11 15:25:05 -0300

    ♻️ Refatorado componente do Spotify

--- src/components/spotify.tsx
+++ src/components/spotify.tsx
@@ -1,9 +1,9 @@
 'use client';
-import { useEffect, useState } from 'react';
 
 import { Pulse } from '@/components/ui/pulse';
 import { SquareArrowOutUpRight } from 'lucide-react';
 import Image from 'next/image';
+import useSWR from 'swr';
 
 interface SpotifyTrack {
   isPlaying: boolean;
@@ -14,68 +14,79 @@ interface SpotifyTrack {
   songUrl: string;
 }
 
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
 export function Spotify() {
-  const [track, setTrack] = useState<SpotifyTrack | null>(null);
+  const { data, isLoading, error } = useSWR<SpotifyTrack>('/api/now-playing', fetcher);
 
-  async function nowPlaying() {
-    const response = await fetch('/api/now-playing');
-    const data = await response.json();
-
-    setTrack(data);
+  if (isLoading) {
+    return (
+      <div className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40">
+        <Image
+          src="/case.png"
+          alt="Capa do álbum"
+          width={400}
+          height={400}
+          className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
+        />
+        <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5" />
+        <h3 className="z-10 text-sm font-medium absolute top-0 left-0 p-4 flex items-center gap-2">
+          <Pulse color="red" />
+          Nada tocando no momento.
+        </h3>
+        <h3 className="z-10 text-sm font-medium absolute bottom-0 left-0 p-4 flex items-center gap-2">
+          Um silêncio confortável.
+        </h3>
+      </div>
+    );
   }
 
-  useEffect(() => {
-    nowPlaying();
-  }, []);
+  if (error) {
+    return (
+      <div className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40">
+        <Image
+          src="/case.png"
+          alt="Capa do álbum"
+          width={400}
+          height={400}
+          className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
+        />
+        <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5" />
+        <h3 className="z-10 text-sm font-medium absolute top-0 left-0 p-4 flex items-center gap-2">
+          <Pulse color="red" />
+          Algo deu errado.
+        </h3>
+        <h3 className="z-10 text-sm font-medium absolute bottom-0 left-0 p-4 flex items-center gap-2">
+          Tente novamente mais tarde.
+        </h3>
+      </div>
+    );
+  }
 
   return (
-    <>
-      {track && track.isPlaying ? (
-        <div className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40">
-          <Image
-            src={track?.albumImageUrl}
-            alt={`Capa do álbum ${track?.album}`}
-            width={400}
-            height={400}
-            className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
-          />
-          <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5" />
-          <h3 className="z-10 text-sm font-medium absolute top-0 left-0 p-4 flex items-center gap-2">
-            <Pulse />
-            {track?.title || 'Local não definido'}, {track?.artist}
-          </h3>
-          <h3 className="z-10 text-sm font-medium absolute bottom-0 left-0 p-4 flex items-center gap-2">
-            {track?.album}
-          </h3>
-          <a
-            href={track?.songUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="z-10 text-sm font-medium absolute bottom-0 right-0 p-4 flex items-center gap-2"
-          >
-            <SquareArrowOutUpRight size={14} />
-            Abrir no Spotify
-          </a>
-        </div>
-      ) : (
-        <div className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40">
-          <Image
-            src="/case.png"
-            alt={`Capa do álbum ${track?.album}`}
-            width={400}
-            height={400}
-            className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
-          />
-          <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5" />
-          <h3 className="z-10 text-sm font-medium absolute top-0 left-0 p-4 flex items-center gap-2">
-            <Pulse color="red" />
-            Nada tocando no momento.
-          </h3>
-          <h3 className="z-10 text-sm font-medium absolute bottom-0 left-0 p-4 flex items-center gap-2">
-            Um silêncio confortável.
-          </h3>
-        </div>
-      )}
-    </>
+    <div className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40">
+      <Image
+        src={data?.albumImageUrl || '/case.png'}
+        alt={`Capa do álbum ${data?.album}`}
+        width={400}
+        height={400}
+        className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
+      />
+      <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5" />
+      <h3 className="z-10 text-sm font-medium absolute top-0 left-0 p-4 flex items-center gap-2">
+        <Pulse />
+        {data?.title || 'Local não definido'}, {data?.artist}
+      </h3>
+      <h3 className="z-10 text-sm font-medium absolute bottom-0 left-0 p-4 flex items-center gap-2">{data?.album}</h3>
+      <a
+        href={data?.songUrl}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="z-10 text-sm font-medium absolute bottom-0 right-0 p-4 flex items-center gap-2"
+      >
+        <SquareArrowOutUpRight size={14} />
+        Abrir no Spotify
+      </a>
+    </div>
   );
 }
