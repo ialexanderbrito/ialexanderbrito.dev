94e0d630431d782800194af2ea00b76298645dea
ialexanderbrito <xanderzinho26@gmail.com>
2025-02-28 11:44:42 -0300

    🐛 Ajustado Github Calendar para ano atual

--- src/components/github-calendar.tsx
+++ src/components/github-calendar.tsx
@@ -1,11 +1,18 @@
 'use client';
+import { useEffect, useState } from 'react';
 import GitHubCalendar from 'react-github-calendar';
 
 import { useTheme } from 'next-themes';
 
 export function GithubCalendar() {
   const { theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const currentYear = new Date().getFullYear();
   const colorScheme = theme === 'dark' ? 'dark' : 'light';
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   const labels = {
     legend: {
@@ -20,17 +27,23 @@ export function GithubCalendar() {
     dark: ['#383838', '#606060', '#8c8c8c', '#bababa', '#ebebeb'],
   };
 
+  if (!mounted) {
+    return null;
+  }
+
   return (
-    <div className="overflow-x-auto w-full">
-      <GitHubCalendar
-        hideMonthLabels
-        blockMargin={6}
-        theme={explicitTheme}
-        year={new Date().getFullYear()}
-        labels={labels}
-        colorScheme={colorScheme}
-        username="ialexanderbrito"
-      />
+    <div className="w-full">
+      <div className="overflow-x-auto">
+        <GitHubCalendar
+          hideMonthLabels
+          blockMargin={6}
+          theme={explicitTheme}
+          labels={labels}
+          colorScheme={colorScheme}
+          username="ialexanderbrito"
+          year={currentYear}
+        />
+      </div>
     </div>
   );
 }
