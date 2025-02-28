0f9351b1dee93224ed0c86e5a66618cbded7236e
ialexanderbrito <xanderzinho26@gmail.com>
2025-02-28 11:44:22 -0300

    🐛 Ajuste de componente ui

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
 import * as React from 'react';
 import { Controller, ControllerProps, FieldPath, FieldValues, FormProvider, useFormContext } from 'react-hook-form';
 
