6f9b359dae1cb077cbd55b6725879e452b1cd6d8
ialexanderbrito <xanderzinho26@gmail.com>
2025-05-02 12:30:39 -0300

    ✨ Adicionado o componente HomeBlurText para exibir texto com animação de desfoque, melhorando a estética e a experiência do usuário

---
+++ src/components/home-blur-text.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import BlurText from '@/components/ui/blur-text';
+import { toast } from '@/hooks';
+
+interface HomeBlurTextProps {
+  text: string;
+  className?: string;
+}
+
+export default function HomeBlurText({ text, className }: HomeBlurTextProps) {
+  return (
+    <BlurText
+      text={text}
+      delay={250}
+      animateBy="letters"
+      direction="top"
+      className={className}
+      onAnimationComplete={() => {
+        console.log('Animation complete');
+      }}
+    />
+  );
+}
