cfa246148093f8d4b1e0ecbae8fdef8b6df1195e
ialexanderbrito <xanderzinho26@gmail.com>
2025-05-07 10:46:57 -0300

    ✨ Adicionada lógica de tema ao componente ShinyText, melhorando a estilização com base no tema claro ou escuro.

--- src/components/ui/shiny-text.tsx
+++ src/components/ui/shiny-text.tsx
@@ -1,6 +1,9 @@
+'use client';
+
 import React from 'react';
 
 import { cn } from '@/lib/utils';
+import { useTheme } from 'next-themes';
 
 interface ShinyTextProps {
   text: string;
@@ -9,20 +12,34 @@ interface ShinyTextProps {
   className?: string;
 }
 
-const ShinyText: React.FC<ShinyTextProps> = ({ text, disabled = false, speed = 5, className = '' }) => (
-  <div
-    className={cn(
-      'bg-clip-text text-[#b5b5b5a4] inline-block',
-      !disabled && 'animate-shine',
-      disabled && 'shiny-text disabled',
-      className,
-    )}
-    style={{
-      animationDuration: `${speed}s`,
-    }}
-  >
-    {text}
-  </div>
-);
+const ShinyText: React.FC<ShinyTextProps> = ({ text, disabled = false, speed = 5, className = '' }) => {
+  const { theme } = useTheme();
+  const isLightTheme = theme === 'light';
+
+  return (
+    <div
+      className={cn(
+        'bg-clip-text inline-block',
+        !disabled && 'animate-shine',
+        isLightTheme ? 'text-[#505050a4]' : 'text-[#b5b5b5a4]',
+        disabled && 'shiny-text disabled',
+        className,
+      )}
+      style={{
+        animationDuration: `${speed}s`,
+        backgroundImage: `linear-gradient(
+          to right,
+          ${isLightTheme && '#505050'} 20%,
+          ${isLightTheme && '#000000'} 40%,
+          ${isLightTheme && '#000000'} 60%,
+          ${isLightTheme && '#505050'} 80%
+        )`,
+        // backgroundSize: '200% auto',
+      }}
+    >
+      {text}
+    </div>
+  );
+};
 
 export default ShinyText;
