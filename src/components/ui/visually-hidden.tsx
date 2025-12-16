cbb4e1e8885efb59f6dd5298f5761734d352150b
ialexanderbrito <xanderzinho26@gmail.com>
2025-12-16 17:09:20 -0300

    ✨ Melhorada a legibilidade do código em visually-hidden.tsx, alterando a interface para um tipo e ajustando a formatação do componente.

--- src/components/ui/visually-hidden.tsx
+++ src/components/ui/visually-hidden.tsx
@@ -2,19 +2,21 @@ import React from 'react';
 
 import { cn } from '@/lib/utils';
 
-interface VisuallyHiddenProps extends React.HTMLAttributes<HTMLSpanElement> {}
+type VisuallyHiddenProps = React.HTMLAttributes<HTMLSpanElement>;
 
-const VisuallyHidden = React.forwardRef<HTMLSpanElement, VisuallyHiddenProps>(({ className, ...props }, ref) => (
-  <span
-    ref={ref}
-    className={cn(
-      'absolute w-[1px] h-[1px] p-0 -m-[1px] overflow-hidden whitespace-nowrap',
-      'border-0 clip-rect-0',
-      className,
-    )}
-    {...props}
-  />
-));
+const VisuallyHidden = React.forwardRef<HTMLSpanElement, VisuallyHiddenProps>(
+  ({ className, ...props }, ref) => (
+    <span
+      ref={ref}
+      className={cn(
+        'absolute w-px h-px p-0 -m-px overflow-hidden whitespace-nowrap',
+        'border-0 clip-rect-0',
+        className,
+      )}
+      {...props}
+    />
+  ),
+);
 
 VisuallyHidden.displayName = 'VisuallyHidden';
 
