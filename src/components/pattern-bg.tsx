3cca8c8c424a02d42bc9bdc302c9a5f9ecea5d50
ialexanderbrito <xanderzinho26@gmail.com>
2025-12-16 17:08:29 -0300

    ✨ Melhorada a legibilidade do código em pattern-bg.tsx, ajustando a formatação das tags SVG e propriedades.

--- src/components/pattern-bg.tsx
+++ src/components/pattern-bg.tsx
@@ -1,5 +1,10 @@
 const Checkered = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="100"
+    height="100"
+    viewBox="0 0 100 100"
+  >
     <g fillRule="evenodd">
       <g className="fill-muted-foreground">
         <path
@@ -12,7 +17,16 @@ const Checkered = () => (
   </svg>
 );
 
-const Dots = () => <rect x="0" y="0" width="4" height="4" className="text-muted-foreground" fill="currentColor" />;
+const Dots = () => (
+  <rect
+    x="0"
+    y="0"
+    width="4"
+    height="4"
+    className="text-muted-foreground"
+    fill="currentColor"
+  />
+);
 
 type PatternProps = { variant: 'dots' | 'checkered' };
 
@@ -26,9 +40,19 @@ export const PatternBackground = ({ variant }: PatternProps) => {
     <>
       <div className="absolute right-0 top-0 -z-40 h-screen w-screen bg-linear-to-b from-transparent to-background" />
 
-      <svg className="absolute right-0 top-0 -z-50 h-screen w-screen opacity-10 dark:opacity-5" fill="none">
+      <svg
+        className="absolute right-0 top-0 -z-50 h-screen w-screen opacity-10 dark:opacity-5"
+        fill="none"
+      >
         <defs>
-          <pattern id={variant} x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+          <pattern
+            id={variant}
+            x="0"
+            y="0"
+            width="20"
+            height="20"
+            patternUnits="userSpaceOnUse"
+          >
             {pattern[variant]()}
           </pattern>
         </defs>
