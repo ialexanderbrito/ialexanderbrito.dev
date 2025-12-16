615f6585cfb50a9d9fb5e196148a01fe0f537cc6
ialexanderbrito <xanderzinho26@gmail.com>
2025-12-16 17:04:55 -0300

    ✨ Melhorada a legibilidade do código em dialog-image.tsx, ajustando a formatação das importações e do componente Button.

--- src/components/dialog-image.tsx
+++ src/components/dialog-image.tsx
@@ -1,5 +1,10 @@
 import { Button } from '@/components/ui/button';
-import { Dialog, DialogContent, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 import { VisuallyHidden } from '@/components/ui/visually-hidden';
 import { ZoomIn } from 'lucide-react';
 
@@ -12,7 +17,12 @@ export function DialogImage({ src, alt }: DialogImageProps) {
   return (
     <Dialog>
       <DialogTrigger>
-        <Button variant="link" size="icon" className="absolute top-1 right-1 text-white w-4 h-4" asChild>
+        <Button
+          variant="link"
+          size="icon"
+          className="absolute top-1 right-1 text-white w-4 h-4"
+          asChild
+        >
           <ZoomIn size={18} />
         </Button>
         <img src={src} alt={alt} className="cursor-zoom-in" loading="lazy" />
