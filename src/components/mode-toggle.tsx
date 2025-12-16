1dee86d5327ce716a29eeb32d725c567ee99a9f6
ialexanderbrito <xanderzinho26@gmail.com>
2025-12-16 17:08:10 -0300

    ✨ Melhorada a legibilidade do código em mode-toggle.tsx, ajustando a formatação do texto alternativo.

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -22,7 +22,9 @@ export function ModeToggle() {
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
 
-          <span className="sr-only">Alternar entre os temas claro e escuro</span>
+          <span className="sr-only">
+            Alternar entre os temas claro e escuro
+          </span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
