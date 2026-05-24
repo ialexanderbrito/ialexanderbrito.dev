1588039ddbee7f43bce7a0073010e604e7093864
ialexanderbrito <xanderzinho26@gmail.com>
2026-05-23 23:46:38 -0300

    ✨ Update next.config.js: enable experimental turbopack support and remove swcMinify option

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  experimental: {
+    turbopack: {
+      root: __dirname,
+    },
+  },
   images: {
     remotePatterns: [
       {
