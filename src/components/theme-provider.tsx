2b8783b6ea9ee84534ee4a93331debb15ab5c41a
ialexanderbrito <xanderzinho26@gmail.com>
2026-05-23 23:45:55 -0300

    ✨ Enhance ThemeProvider: add error handling for script tag warnings in development mode

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,8 +1,21 @@
 'use client';
 
-import * as React from 'react';
-
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
+
+if (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') {
+  const origError = console.error;
+  console.error = (...args) => {
+    if (
+      typeof args[0] === 'string' &&
+      args[0].includes(
+        'Encountered a script tag while rendering React component',
+      )
+    ) {
+      return;
+    }
+    origError.apply(console, args);
+  };
+}
 
 export function ThemeProvider({ children, ...props }: any) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
